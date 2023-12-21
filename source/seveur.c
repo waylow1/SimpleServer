1f320b1f93f353c5e86554bae80a727f6f186162
Waterfox <waterfox80@gmail.com>
2023-12-21 10:33:53 +0100

    Add `Matiere` to server

---
+++ source/seveur.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+#include "matiere.c"
+
+Matiere listeMatieres[] =
+{
+    { 1,"Anglais", 12.5},
+    { 2,"Mathematiques", 10.8},
+    { 3,"Informatique", 9.3},
+    { 4,"Culturte generale", 12.6},
+    { 5,"Physiques", 11.8},
+    { 6,"Programmation systeme", 12.3}
+};
